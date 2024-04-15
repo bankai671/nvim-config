f89a04a2f129e9ddfbbec0ca2af8de690d9edc2e
John Doe <bankai671@proton.me>
2024-04-16 00:27:39 +0200

    update keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,6 +6,15 @@ vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<cr>',   { noremap = true })
 -- ToggleTerm keymaps
 vim.keymap.set('n', '<leader>t', ':ToggleTerm<cr>', { noremap = true })
 
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line down(n)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line up(n)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line down(v)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line up(v)
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==") -- move line down(n)
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==") -- move line up(n)
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv") -- move line down(v)
+vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv") -- move line up(v)
+
 vim.cmd [[
     vmap <Tab> >gv
     vmap <S-Tab> <gv
